0f95c6f68b615fe920569b68c46835a8f4189b50
Gautam Gadipudi <gautam.96.gadipudi@gmail.com>
2020-07-01 20:32:42 -0400

    Add service and repo config to startup

--- code-challenge/Models/Compensation.cs
+++ code-challenge/Models/Compensation.cs
@@ -3,6 +3,7 @@ namespace challenge.Models
 {
     public class Compensation
     {
+        public String CompensationId { get; set; }
         public Employee Employee { get; set; }
         public int Salary { get; set; }
         public String EffectiveDate { get; set; }
